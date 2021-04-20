d5540450b993d785632c7abf825d1b04dc3a6c22
charles <charlesclinton2003@gmail.com>
2021-04-20 12:02:48 +0100

    added environment variables and implemtenting the api

---
+++ api/models/User.go
@@ -0,0 +1,67 @@
+package models
+
+import (
+	"errors"
+	"github.com/badoux/checkmail"
+	"golang.org/x/crypto/bcrypt"
+	"html"
+	"strings"
+	"time"
+)
+
+type User struct {
+	ID uint32 `gorm:"primary_key;auto_increment" json:"id"`
+	Nickname string `gorm:"size:255;not null;unique" json:"nickname"`
+	Email string `gorm:"size:100;not null;unique" json:"email"`
+	Password string `gorm:"size:100;not null;" json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
+
+func Hash(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func VerifyPassword(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
+
+func (u *User) BeforeSave() error {
+	hashedPassword, err := Hash(u.Password)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) Prepare() {
+	u.ID = 0
+	u.Nickname = html.EscapeString(strings.TrimSpace(u.Nickname))
+	u.Email = html.EscapeString(strings.TrimSpace(u.Email))
+	u.CreatedAt = time.Now()
+	u.UpdatedAt = time.Now()
+}
+
+func (u *User) Validate(action string) error {
+	switch strings.ToLower(action) {
+	case "update":
+		if u.Nickname == "" {
+			return errors.New("required nickname")
+		}
+		if u.Password == "" {
+			return errors.New("required password")
+		}
+		if u.Email == "" {
+			return errors.New("required email")
+		}
+		if err := checkmail.ValidateFormat(u.Email); err != nil {
+			return errors.New("invalid email")
+		}
+		return nil
+	case "login":
+		if u.Password == "" {
+			return errors.New("required passwo")
+		}
+	}
+}
